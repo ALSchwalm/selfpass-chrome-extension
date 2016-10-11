81631172ee51d1433ef39091be56237496a20976
Adam Schwalm <adamschwalm@gmail.com>
2016-10-10 22:35:24 -0500

    Move .jsx files to jsx folder

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 ./node_modules/.bin/watchify -v -t [ babelify ] src/js/selfpass.js -o build/js/selfpass-bundle.js &
-./node_modules/.bin/watchify -v -t [ babelify ] src/js/content.jsx -o build/js/content-bundle.js &
-./node_modules/.bin/watchify -v -t [ babelify ] src/js/popup.jsx -o build/js/popup-bundle.js &
-./node_modules/.bin/watchify -v -t [ babelify ] src/js/fill-popup.jsx -o build/js/fill-popup-bundle.js &
-./node_modules/.bin/watchify -v -t [ babelify ] src/js/generate-popup.jsx -o build/js/generate-popup-bundle.js &
-./node_modules/.bin/watchify -v -t [ babelify ] src/js/keystore.jsx -o build/js/keystore-bundle.js &
+./node_modules/.bin/watchify -v -t [ babelify ] src/jsx/content.jsx -o build/js/content-bundle.js &
+./node_modules/.bin/watchify -v -t [ babelify ] src/jsx/popup.jsx -o build/js/popup-bundle.js &
+./node_modules/.bin/watchify -v -t [ babelify ] src/jsx/fill-popup.jsx -o build/js/fill-popup-bundle.js &
+./node_modules/.bin/watchify -v -t [ babelify ] src/jsx/generate-popup.jsx -o build/js/generate-popup-bundle.js &
+./node_modules/.bin/watchify -v -t [ babelify ] src/jsx/keystore.jsx -o build/js/keystore-bundle.js &
 
 for job in `jobs -p`
 do
--- src/js/selfpass.js
+++ src/js/selfpass.js
@@ -3,71 +3,11 @@ import superagent from "superagent";
 import SuperagentPromise from "superagent-promise";
 import ChromePromise from "chrome-promise";
 
+import Keystore from "./keystore.js";
 import cryptography from "./cryptography.js";
 
 const agent = SuperagentPromise(superagent, Promise);
 const chromep = new ChromePromise();
-
-class Keystore {
-  constructor(store) {
-    if (typeof(store) === "undefined") {
-      this.store = {};
-    } else {
-      this.store = store;
-    }
-  }
-
-  parseURI(uri) {
-    const parser = document.createElement('a');
-    parser.href = uri;
-    return parser;
-  }
-
-  addCredentials(uri, username, password, favicon) {
-    const host = this.parseURI(uri).host;
-    const entry = {
-      host: host,
-      uri: uri,
-      username: username,
-      password: password,
-      favicon: favicon,
-      time: new Date()
-    };
-
-    if (typeof(this.store[host]) === "undefined") {
-      this.store[host] = {};
-    }
-    if (typeof(this.store[host][username] === "undefined")) {
-      this.store[host][username] = [];
-    }
-    this.store[host][username].push(entry);
-  }
-
-  credentialsForUrl(uri) {
-    const host = this.parseURI(uri).host;
-    if (typeof(this.store[host]) === "undefined") {
-      return [];
-    }
-
-    var currentCredentials = [];
-    for(const username in this.store[host]) {
-      const history = this.store[host][username];
-      currentCredentials.push(history[history.length-1]);
-    }
-    return currentCredentials;
-  }
-
-  serialize() {
-    return JSON.stringify(this.store);
-  }
-
-  removeSite(uri){
-    const host = this.parseURI(uri).host;
-
-    // This should be removed after the next merge
-    this.store[host] = null;
-  }
-}
 
 var selfpass = (function(){
   var state = {
